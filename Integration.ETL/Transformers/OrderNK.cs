4015cbc133bf6f2f2f2c6d9438485963fcbf5ecb
hugoageont <hugo.garcia@ontica.org>
2025-03-27 15:49:44 -0600

    Delete unused DataFields in Order nk

--- Integration.ETL/Transformers/OrderNK.cs
+++ Integration.ETL/Transformers/OrderNK.cs
@@ -24,17 +24,7 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal DateTime Fecha {
       get; set;
     }
-    /*
-    [DataField("FECHA_ENTREGA")]
-    internal DateTime Fecha_Entrega {
-      get; set;
-    }
-
-    [DataField("FECHA_PROMESA")]
-    internal DateTime Fecha_Promesa {
-      get; set;
-    }*/
-
+ 
     [DataField("CLIENTE")]
     internal string Cliente {
       get; set;
@@ -44,12 +34,7 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal string SubCliente {
       get; set;
     }
-    /*
-    [DataField("ENTREGA")]
-    internal int Entrega {
-      get; set;
-    }*/
-
+ 
     [DataField("ALMACEN")]
     internal string Almacen {
       get; set;
@@ -59,32 +44,12 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal string Vendedor {
       get; set;
     }
-    /*
-    [DataField("LISTAPRECIOS")]
-    internal int ListaPrecios {
-      get; set;
-    }*/
-
+  
     [DataField("MONEDA")]
     internal string Moneda {
       get; set;
     }
-    /*
-    [DataField("CONDICIONDEPAGO")]
-    internal string CondicionDePago {
-      get; set;
-    }
-
-    [DataField("VIAEMBARQUE")]
-    internal string ViaEmbarque {
-      get; set;
-    }
-
-    [DataField("BACKORDER")]
-    internal string BackOrder {
-      get; set;
-    }*/
-
+ 
     [DataField("PRIORIDAD")]
     internal string Prioridad {
       get; set;
@@ -94,22 +59,7 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal string Orden {
       get; set;
     }
-    /*
-    [DataField("SUBTOTAL")]
-    internal decimal SubTotal {
-      get; set;
-    }
-
-    [DataField("IVA")]
-    internal decimal IVA {
-      get; set;
-    }
-
-    [DataField("TOTAL")]
-    internal decimal Total {
-      get; set;
-    }*/
-
+ 
     [DataField("APLICADO")]
     internal string Aplicado {
       get; set;
@@ -124,87 +74,12 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal string Estatus {
       get; set;
     }
-    /*
-    [DataField("VENCIMIENTOS")]
-    internal string Vencimientos {
-      get; set;
-    }*/
-    /*
-    [DataField("IEPS")]
-    internal decimal Ieps {
-      get; set;
-    }*/
-    /*
-    [DataField("ESTATUS_CREDITO")]
-    internal string Estatus_Credito {
-      get; set;
-    }
-    *//*
-    [DataField("USUARIO_APLICO")]
-    internal string Usuario_Aplico {
-      get; set;
-    }
-
-    [DataField("FECHA_APLICO")]
-    internal DateTime Fecha_Aplico {
-      get; set;
-    }
-
-    [DataField("INI_CAPTURA")]
-    internal DateTime Ini_Captura {
-      get; set;
-    }
-
-    [DataField("USR_AUTORIZA")]
-    internal string Usr_Autoriza {
-      get; set;
-    }
-
-    [DataField("FIN_AUTORIZA")]
-    internal DateTime Fin_Autoriza {
-      get; set;
-    }*/
-
+  
     [DataField("USR_CAPTURA")]
     internal string Usr_Captura {
       get; set;
     }
-    /*
-    [DataField("TELEMARKETER")]
-    internal string Telemarketer {
-      get; set;
-    }
-
-    [DataField("FV_FIJA")]
-    internal DateTime Fv_Fija {
-      get; set;
-    }
-    
-    [DataField("USOCFDI")]
-    internal string UsoCFDI {
-      get; set;
-    }
-
-    [DataField("SAT_METODOPAGO")]
-    internal string SAT_Metodopago {
-      get; set;
-    }
-
-    [DataField("SAT_FORMAPAGO")]
-    internal string SAT_Formapago {
-      get; set;
-    }
-    
-    [DataField("PROMOVENTA")]
-    internal string Promoventa {
-      get; set;
-    }
-    
-    [DataField("SAT_REGIMENFISCAL")]
-    internal string SAT_Regimenfiscal {
-      get; set;
-    }
-    */
+   
     [DataField("BinaryChecksum")]
     internal int BinaryChecksum {
       get; set;
@@ -214,17 +89,7 @@ namespace Empiria.Trade.Integration.ETL.Transformers {
     internal int OldBinaryChecksum {
       get; set;
     }
-    /*
-    [DataField("FECHA_CIERRE")]
-    internal DateTime Fecha_Cierre {
-      get; set;
-    }
-
-    [DataField("USUARIO_CIERRE")]
-    internal string Usr_Cierre {
-      get; set;
-    }*/
-
+  
   }  // class OrderNK
 
 }  // namespace Empiria.Trade.Integration.ETL.Transformers
